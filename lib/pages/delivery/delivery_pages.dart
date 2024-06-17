85a60f90dc052451391bbcd69283ddae12a9a79a
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-06-17 18:33:24 +0700

    feat: create delivery_pages.dart and order_pages.dart

---
+++ lib/pages/delivery/delivery_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DeliveryPages extends StatefulWidget {
+  const DeliveryPages({super.key});
+
+  @override
+  State<DeliveryPages> createState() => _DeliveryPagesState();
+}
+
+class _DeliveryPagesState extends State<DeliveryPages> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
